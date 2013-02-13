akrieger/Cataclysm-DDA

commit ca3377a7d1d7f16dbd8f85be9c5ed1e02dd979f6

Fork off place_item terrain flag from container flag so container can just be item visibility.

mapdata.h
@@ -34,6 +34,7 @@ enum t_flag {
  transparent = 0,// Player & monsters can see through/past it
  bashable,     // Player & monsters can bash this & make it the next in the list
  container,    // Items on this square are hidden until looted by the player
+ place_item,   // Valid terrain for place_item() to put items on
  door,         // Can be opened--used for NPC pathfinding.
  flammable,    // May be lit on fire
  l_flammable,  // Harder to light on fire, but still possible
@@ -183,9 +184,9 @@ const ter_t terlist[num_terrain_types] = {  // MUST match enum ter_id above!
 {"pile of ash",   '#', c_ltgray,  2, tr_null,
 	mfb(transparent)|mfb(diggable)},
 {"twisted metal",    '#', c_cyan,    5, tr_null,
-	mfb(transparent)|mfb(rough)|mfb(sharp)|mfb(container)},
+        mfb(transparent)|mfb(rough)|mfb(sharp)|mfb(place_item)},
 {"metal wreckage",   '#', c_cyan,    5, tr_null,
-	mfb(transparent)|mfb(rough)|mfb(sharp)|mfb(container)},
+        mfb(transparent)|mfb(rough)|mfb(sharp)|mfb(place_item)},
 {"grass",	     '.', c_green,   2, tr_null,
 	mfb(transparent)|mfb(diggable)|mfb(tentable)},
 {"metal floor",      '.', c_ltcyan,  2, tr_null,
@@ -201,7 +202,7 @@ const ter_t terlist[num_terrain_types] = {  // MUST match enum ter_id above!
 {"metal grate",      '#', c_dkgray,  2, tr_null,
 	mfb(transparent)},
 {"slime",            '~', c_green,   6, tr_null,
-	mfb(transparent)|mfb(container)|mfb(flammable2)},
+        mfb(transparent)|mfb(container)|mfb(flammable2)|mfb(place_item)},
 {"walkway",          '#', c_yellow,  2, tr_null,
 	mfb(transparent)},
 {"canvas wall",      '#', c_blue,   0, tr_null,
@@ -228,21 +229,21 @@ const ter_t terlist[num_terrain_types] = {  // MUST match enum ter_id above!
 	mfb(transparent)|mfb(bashable)|mfb(flammable2)|mfb(noitem)|
         mfb(supports_roof)},
 {"wall",             '|', c_ltgray,  0, tr_null,
- mfb(flammable)|mfb(noitem)|mfb(supports_roof)},
+        mfb(flammable)|mfb(noitem)|mfb(supports_roof)},
 {"wall",             '-', c_ltgray,  0, tr_null,
- mfb(flammable)|mfb(noitem)|mfb(supports_roof)},
+        mfb(flammable)|mfb(noitem)|mfb(supports_roof)},
 {"concrete wall",    '|', c_dkgray,  0, tr_null,
- mfb(noitem)|mfb(supports_roof)},
+        mfb(noitem)|mfb(supports_roof)},
 {"concrete wall",    '-', c_dkgray,  0, tr_null,
- mfb(noitem)|mfb(supports_roof)},
+        mfb(noitem)|mfb(supports_roof)},
 {"metal wall",       '|', c_cyan,    0, tr_null,
- mfb(noitem)|mfb(noitem)|mfb(supports_roof)},
+        mfb(noitem)|mfb(noitem)|mfb(supports_roof)},
 {"metal wall",       '-', c_cyan,    0, tr_null,
- mfb(noitem)|mfb(noitem)|mfb(supports_roof)},
+        mfb(noitem)|mfb(noitem)|mfb(supports_roof)},
 {"glass wall",       '|', c_ltcyan,  0, tr_null,
- mfb(transparent)|mfb(bashable)|mfb(noitem)|mfb(supports_roof)},
+        mfb(transparent)|mfb(bashable)|mfb(noitem)|mfb(supports_roof)},
 {"glass wall",       '-', c_ltcyan,  0, tr_null,
- mfb(transparent)|mfb(bashable)|mfb(noitem)|mfb(supports_roof)},
+        mfb(transparent)|mfb(bashable)|mfb(noitem)|mfb(supports_roof)},
 {"glass wall",       '|', c_ltcyan,  0, tr_null, // Alarmed
 	mfb(transparent)|mfb(bashable)|mfb(alarmed)|mfb(noitem)|
  mfb(supports_roof)},
@@ -250,9 +251,9 @@ const ter_t terlist[num_terrain_types] = {  // MUST match enum ter_id above!
 	mfb(transparent)|mfb(bashable)|mfb(alarmed)|mfb(noitem)|
  mfb(supports_roof)},
 {"reinforced glass", '|', c_ltcyan,  0, tr_null,
- mfb(transparent)|mfb(bashable)|mfb(noitem)|mfb(supports_roof)},
+        mfb(transparent)|mfb(bashable)|mfb(noitem)|mfb(supports_roof)},
 {"reinforced glass", '-', c_ltcyan,  0, tr_null,
- mfb(transparent)|mfb(bashable)|mfb(noitem)|mfb(supports_roof)},
+        mfb(transparent)|mfb(bashable)|mfb(noitem)|mfb(supports_roof)},
 {"metal bars",       '"', c_ltgray,  0, tr_null,
 	mfb(transparent)|mfb(noitem)},
 {"closed wood door", '+', c_brown,   0, tr_null,
@@ -265,8 +266,7 @@ const ter_t terlist[num_terrain_types] = {  // MUST match enum ter_id above!
 {"closed wood door", '+', c_brown,   0, tr_null,	// Actually locked
         mfb(bashable)|mfb(flammable2)|mfb(noitem)|mfb(supports_roof)},
 {"closed wood door", '+', c_brown,   0, tr_null, // Locked and alarmed
-	mfb(bashable)|mfb(flammable2)|mfb(alarmed)|mfb(noitem)|
- mfb(supports_roof)},
+	mfb(bashable)|mfb(flammable2)|mfb(alarmed)|mfb(noitem)|mfb(supports_roof)},
 {"empty door frame", '.', c_brown,   2, tr_null,
         mfb(transparent)|mfb(supports_roof)},
 {"locked wire gate", '+', c_cyan,   0, tr_null,
@@ -327,15 +327,16 @@ const ter_t terlist[num_terrain_types] = {  // MUST match enum ter_id above!
 	mfb(transparent)|mfb(bashable)|mfb(flammable2)|mfb(noitem)},
 {"underbrush",       '#', c_ltgreen, 6, tr_null,
 	mfb(transparent)|mfb(bashable)|mfb(diggable)|mfb(container)|
-	mfb(flammable2)|mfb(thin_obstacle)},
+        mfb(flammable2)|mfb(thin_obstacle)|mfb(place_item)},
 {"shrub",            '#', c_green,   8, tr_null,
-	mfb(transparent)|mfb(bashable)|mfb(container)|mfb(flammable2)|mfb(thin_obstacle)},
+        mfb(transparent)|mfb(bashable)|mfb(container)|mfb(flammable2)|
+        mfb(thin_obstacle)|mfb(place_item)},
 {"tree trunk",              '1', c_brown,   4, tr_null,
 	mfb(transparent)|mfb(flammable2)|mfb(diggable)},
 {"root wall",        '#', c_brown,   0, tr_null,
 	mfb(noitem)|mfb(supports_roof)},
 {"wax wall",         '#', c_yellow,  0, tr_null,
-	mfb(container)|mfb(flammable2)|mfb(noitem)|mfb(supports_roof)},
+        mfb(flammable2)|mfb(noitem)|mfb(supports_roof)|mfb(place_item)},
 {"wax floor",        '.', c_yellow,  2, tr_null,
 	mfb(transparent)|mfb(l_flammable)},
 {"picket fence",     '|', c_brown,   3, tr_null,
@@ -375,27 +376,30 @@ const ter_t terlist[num_terrain_types] = {  // MUST match enum ter_id above!
 {"lava",             '~', c_red,     4, tr_lava,
 	mfb(transparent)|mfb(liquid)},
 {"bed",              '#', c_magenta, 5, tr_null,
-	mfb(transparent)|mfb(container)|mfb(flammable2)|mfb(collapses)|mfb(deconstruct)},
+        mfb(transparent)|mfb(container)|mfb(flammable2)|mfb(collapses)|
+        mfb(deconstruct)|mfb(place_item)},
 {"toilet",           '&', c_white,   4, tr_null,
 	mfb(transparent)|mfb(bashable)|mfb(l_flammable)|mfb(collapses)},
 {"makeshift bed",    '#', c_magenta, 5, tr_null,
         mfb(transparent)|mfb(bashable)|mfb(flammable2)|mfb(collapses)|mfb(deconstruct)},
 {"sink",             '&', c_white,   4, tr_null,
-        mfb(transparent)|mfb(bashable)|mfb(l_flammable)|mfb(collapses)},
+        mfb(transparent)|mfb(bashable)|mfb(l_flammable)|mfb(collapses)|mfb(container)|mfb(place_item)},
 {"oven",             '#', c_white,   4, tr_null,
-        mfb(transparent)|mfb(bashable)|mfb(l_flammable)|mfb(collapses)|mfb(container)},
+        mfb(transparent)|mfb(bashable)|mfb(l_flammable)|mfb(collapses)|mfb(container)|mfb(place_item)},
 {"bathtub",          '~', c_white,   4, tr_null,
-        mfb(transparent)|mfb(bashable)|mfb(l_flammable)|mfb(collapses)|mfb(container)},
+        mfb(transparent)|mfb(bashable)|mfb(l_flammable)|mfb(collapses)|mfb(container)|mfb(place_item)},
 {"chair",            '#', c_brown,   2, tr_null,
 	mfb(transparent)|mfb(flammable2)|mfb(collapses)|mfb(deconstruct)},
 {"chair",            '#', c_ltred,   3, tr_null,
 	mfb(transparent)|mfb(flammable2)|mfb(collapses)|mfb(deconstruct)},
 {"cupboard",         '#', c_blue,    3, tr_null,
-	mfb(transparent)|mfb(flammable2)|mfb(collapses)|mfb(deconstruct)|mfb(container)},
+        mfb(transparent)|mfb(flammable2)|mfb(collapses)|mfb(deconstruct)|
+        mfb(container)|mfb(place_item)},
 {"trash can",        '&', c_ltcyan,  3, tr_null,
-	mfb(transparent)|mfb(flammable2)|mfb(collapses)},
+        mfb(transparent)|mfb(flammable2)|mfb(collapses)|mfb(container)|mfb(place_item)},
 {"desk",             '#', c_brown,   3, tr_null,
-	mfb(transparent)|mfb(flammable2)|mfb(collapses)|mfb(deconstruct)|mfb(container)},
+        mfb(transparent)|mfb(flammable2)|mfb(collapses)|mfb(deconstruct)|
+        mfb(container)|mfb(place_item)},
 {"sandbox", '#', c_yellow, 3, tr_null,
 	mfb(transparent)|mfb(deconstruct)},
 {"slide",            '#', c_ltcyan,  4, tr_null,
@@ -441,36 +445,38 @@ const ter_t terlist[num_terrain_types] = {  // MUST match enum ter_id above!
 {"refrigerator",     '{', c_ltcyan,  0, tr_null,
 	mfb(container)|mfb(collapses)|mfb(bashable)},
 {"dresser",          '{', c_brown,   0, tr_null,
-	mfb(transparent)|mfb(container)|mfb(flammable)|mfb(collapses)|mfb(bashable)|mfb(deconstruct)},
+        mfb(transparent)|mfb(container)|mfb(flammable)|mfb(collapses)|
+        mfb(bashable)|mfb(deconstruct)|mfb(place_item)},
 {"locker",           '{', c_ltgray,  0, tr_null,
-	mfb(container)|mfb(collapses)|mfb(bashable)},
+        mfb(container)|mfb(collapses)|mfb(bashable)|mfb(place_item)},
 {"display rack",     '{', c_ltgray,  0, tr_null,
-        mfb(transparent)|mfb(container)|mfb(l_flammable)|mfb(collapses)|mfb(bashable)|mfb(deconstruct)},
+        mfb(transparent)|mfb(l_flammable)|mfb(collapses)|mfb(bashable)|
+        mfb(deconstruct)|mfb(place_item)},
 {"book case",        '{', c_brown,   0, tr_null,
-	mfb(container)|mfb(flammable)|mfb(collapses)|mfb(bashable)|mfb(deconstruct)},
+        mfb(flammable)|mfb(collapses)|mfb(bashable)|mfb(deconstruct)|mfb(place_item)},
 {"dumpster",	     '{', c_green,   0, tr_null,
-	mfb(container)|mfb(bashable)},
+        mfb(container)|mfb(bashable)|mfb(place_item)},
 {"cloning vat",      '0', c_ltcyan,  0, tr_null,
-	mfb(transparent)|mfb(bashable)|mfb(container)|mfb(sealed)},
+        mfb(transparent)|mfb(bashable)|mfb(container)|mfb(sealed)|mfb(place_item)},
 {"crate",            '{', c_brown,   0, tr_null,
         mfb(transparent)|mfb(bashable)|mfb(container)|mfb(sealed)|
-        mfb(flammable)|mfb(deconstruct)},
+        mfb(flammable)|mfb(deconstruct)|mfb(place_item)},
 {"open crate",       '{', c_brown,   0, tr_null,
-        mfb(transparent)|mfb(bashable)|mfb(container)|mfb(flammable)},
+        mfb(transparent)|mfb(bashable)|mfb(container)|mfb(flammable)|mfb(place_item)},
 {"stairs down",      '>', c_yellow,  2, tr_null,
-	mfb(transparent)|mfb(goes_down)|mfb(container)},
+        mfb(transparent)|mfb(goes_down)|mfb(place_item)},
 {"stairs up",        '<', c_yellow,  2, tr_null,
-	mfb(transparent)|mfb(goes_up)|mfb(container)},
+        mfb(transparent)|mfb(goes_up)||mfb(place_item)},
 {"manhole",          '>', c_dkgray,  2, tr_null,
-	mfb(transparent)|mfb(goes_down)|mfb(container)},
+        mfb(transparent)|mfb(goes_down)|mfb(place_item)},
 {"ladder",           '<', c_dkgray,  2, tr_null,
-	mfb(transparent)|mfb(goes_up)|mfb(container)},
+        mfb(transparent)|mfb(goes_up)|mfb(place_item)},
 {"ladder",           '>', c_dkgray,  2, tr_null,
-	mfb(transparent)|mfb(goes_down)|mfb(container)},
+        mfb(transparent)|mfb(goes_down)|mfb(place_item)},
 {"downward slope",   '>', c_brown,   2, tr_null,
-	mfb(transparent)|mfb(goes_down)|mfb(container)},
+        mfb(transparent)|mfb(goes_down)|mfb(place_item)},
 {"upward slope",     '<', c_brown,   2, tr_null,
-	mfb(transparent)|mfb(goes_up)|mfb(container)},
+        mfb(transparent)|mfb(goes_up)|mfb(place_item)},
 {"rope leading up",  '<', c_white,   2, tr_null,
 	mfb(transparent)|mfb(goes_up)},
 {"manhole cover",    '0', c_dkgray,  2, tr_null,
@@ -514,7 +520,7 @@ const ter_t terlist[num_terrain_types] = {  // MUST match enum ter_id above!
 {"checkered switch", '6', c_white,   0, tr_null,
 	mfb(transparent)},
 {"mutated poppy flower", 'f', c_red, 3, tr_null,
-    mfb(transparent)}
+        mfb(transparent)}
 };
 
 enum map_extra {

mapgen.cpp
@@ -6651,7 +6651,7 @@ void map::place_items(items_location loc, int chance, int x1, int y1,
    tries++;
 // Only place on valid terrain
   } while (((terlist[ter(px, py)].movecost == 0 &&
-             !(terlist[ter(px, py)].flags & mfb(container))) ||
+             !(terlist[ter(px, py)].flags & mfb(place_item))) ||
             (!ongrass && (ter(px, py) == t_dirt || ter(px, py) == t_grass))) &&
            tries < 20);
   if (tries < 20) {

commit dc736d78b95082c8893d1b373fb046f522cc0cca

Update mapdata.h
Fix definition of stairs broken in ca3377a7d1d7f16dbd8f85be9c5ed1e02dd979f6.

mapdata.h
@@ -468,7 +468,7 @@ const ter_t terlist[num_terrain_types] = {  // MUST match enum ter_id above!
 {"stairs down",      '>', c_yellow,  2, tr_null,
         mfb(transparent)|mfb(goes_down)|mfb(place_item)},
 {"stairs up",        '<', c_yellow,  2, tr_null,
-        mfb(transparent)|mfb(goes_up)||mfb(place_item)},
+        mfb(transparent)|mfb(goes_up)|mfb(place_item)},
 {"manhole",          '>', c_dkgray,  2, tr_null,
         mfb(transparent)|mfb(goes_down)|mfb(place_item)},
 {"ladder",           '<', c_dkgray,  2, tr_null,