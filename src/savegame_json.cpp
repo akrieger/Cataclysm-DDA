akrieger/Cataclysm-DDA

commit d1628788113e6cc1628b23d62638d9519be1a226

Remove constructor item(JsonObject&)
It forwards to `item:deserialize`, so one can call it directly.
This is now done in `inventory::json_load_items`. The loop there is also simpler.
Exceptions will now go up the stack automatically. This is fine because all JSON related code needs to expect exceptions from malformed JSON.

src/savegame_json.cpp
@@ -1287,14 +1287,11 @@ void inventory::json_save_items(JsonOut &json) const
 
 void inventory::json_load_items(JsonIn &jsin)
 {
-    try {
-        JsonArray ja = jsin.get_array();
-        while ( ja.has_more() ) {
-            JsonObject jo = ja.next_object();
-            add_item(item( jo ), true, false);
-        }
-    } catch( const JsonError &jsonerr ) {
-        debugmsg("bad inventory json:\n%s", jsonerr.c_str() );
+    jsin.start_array();
+    while( !jsin.end_array() ) {
+        item tmp;
+        tmp.deserialize( jsin );
+        add_item( tmp );
     }
 }
 

src/item.cpp
@@ -201,11 +201,6 @@ item item::make_corpse( const mtype_id& mt, int turn, const std::string &name )
     return result;
 }
 
-item::item(JsonObject &jo)
-{
-    deserialize(jo);
-}
-
 item& item::convert( const itype_id& new_type )
 {
     type = find_type( new_type );

src/item.h
@@ -210,8 +210,6 @@ class item : public JsonSerializer, public JsonDeserializer, public visitable<it
         item( const itype_id& id, int turn, solitary_tag );
         item( const itype *type, int turn, solitary_tag );
 
-        item( JsonObject &jo );
-
         /**
          * Filter converting this instance to another type preserving all other aspects
          * @param new_type the type id to convert to

commit a4d2238f55c3b0ee8c88d87a5738e1dc9f17381f

Fix loading item invlets from saves.
d1628788113e6cc1628b23d62638d9519be1a226 accidentally changed the call to just `add_item(tmp)` (without the two boolean values).
This meant the added item would get their default invlet assigned.
This commit restores the orignal behavior.

src/savegame_json.cpp
@@ -1291,7 +1291,7 @@ void inventory::json_load_items(JsonIn &jsin)
     while( !jsin.end_array() ) {
         item tmp;
         tmp.deserialize( jsin );
-        add_item( tmp );
+        add_item( tmp, true, false );
     }
 }
 