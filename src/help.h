5d1cf832aea16e5fd9112e67fe7c59d4e3f77660
Andrew Krieger <akrieger@users.noreply.github.com>
2021-06-11 21:25:49 -0700

    de-jsonin help and minorly convert off streaming JsonIn

--- src/help.h
+++ src/help.h
@@ -7,6 +7,7 @@
 #include <utility>
 #include <vector>
 
+class JsonArray;
 class JsonIn;
 class translation;
 namespace catacurses
@@ -22,6 +23,7 @@ class help
 
     private:
         void deserialize( JsonIn &jsin );
+        void deserialize( const JsonArray &ja );
         void draw_menu( const catacurses::window &win ) const;
         static std::string get_note_colors();
         static std::string get_dir_grid();
