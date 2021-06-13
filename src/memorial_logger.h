6fa595183cab966e8bf25c5b7ea0339a3ac5a30a
Andrew Krieger <akrieger@users.noreply.github.com>
2021-06-12 22:26:21 -0700

    de-jsonin memorial_logger

--- src/memorial_logger.h
+++ src/memorial_logger.h
@@ -17,6 +17,7 @@ namespace cata
 class event;
 }  // namespace cata
 class JsonIn;
+class JsonObject;
 class JsonOut;
 
 class memorial_log_entry
@@ -30,6 +31,7 @@ class memorial_log_entry
         std::string to_string() const;
 
         void deserialize( JsonIn & );
+        void deserialize( const JsonObject &jo );
         void serialize( JsonOut & ) const;
     private:
         time_point time_;
