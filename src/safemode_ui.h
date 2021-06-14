1145f98be4bd75ec7b2f0f2b4db9c045d8c82757
Andrew Krieger <akrieger@users.noreply.github.com>
2021-06-13 17:30:27 -0700

    de-jsonin safemode_ui technically convert off streaming JsonIn

--- src/safemode_ui.h
+++ src/safemode_ui.h
@@ -11,6 +11,7 @@
 #include "creature.h"
 #include "enums.h"
 
+class JsonArray;
 class JsonIn;
 class JsonOut;
 
@@ -111,6 +112,7 @@ class safemode
 
         void serialize( JsonOut &json ) const;
         void deserialize( JsonIn &jsin );
+        void deserialize( const JsonArray &ja );
 };
 
 safemode &get_safemode();
