fbc7febfde693b12c32283cc5de81e4f32b60a38
akrieger <akrieger@user.noreply.github.com>
2021-09-19 15:38:28 -0700

    Convert mm_region::deserialize to JsonValue (slow)

--- src/map_memory.cpp
+++ src/map_memory.cpp
@@ -213,7 +213,7 @@ shared_ptr_fast<mm_submap> map_memory::load_submap( const tripoint &sm_pos )
     }
 
     mm_region mmr;
-    const auto loader = [&]( JsonIn & jsin ) {
+    const auto loader = [&]( const JsonValue & jsin ) {
         mmr.deserialize( jsin );
     };
 
