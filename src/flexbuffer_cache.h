bca4f2abe11417d24de131dc91f755c5d881b8e1
akrieger <akrieger@users.noreply.github.com>
2022-07-03 11:50:49 -0700

    Make flexbuffer_cache assume and use more lexically relative paths.

--- src/flexbuffer_cache.h
+++ src/flexbuffer_cache.h
@@ -59,7 +59,7 @@ class flexbuffer_cache
 
         // Throw exceptions on IO and parse errors.
         shared_flexbuffer parse( fs::path json_source_path, size_t offset = 0 ) noexcept( false );
-        shared_flexbuffer parse_and_cache( fs::path json_source_path,
+        shared_flexbuffer parse_and_cache( fs::path lexically_normal_json_source_path,
                                            size_t offset = 0 ) noexcept( false ) ;
 
         static shared_flexbuffer parse_buffer( std::string buffer ) noexcept( false );
@@ -75,7 +75,6 @@ class flexbuffer_cache
         std::unordered_map<std::string, shared_flexbuffer> cached_buffers_;
 
         // Map of original json file path to disk serialized FlexBuffer path and mtime of input.
-
         std::unique_ptr<flexbuffer_disk_cache> disk_cache_;
 };
 
