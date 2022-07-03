a1d36ac4f54b2604a82be1dd3012beffbc43d1b1
akrieger <akrieger@users.noreply.github.com>
2022-07-03 11:49:12 -0700

    Support global deserialize(T, const JsonValue&)

--- src/activity_actor.cpp
+++ src/activity_actor.cpp
@@ -6092,6 +6092,7 @@ void serialize( const cata::clone_ptr<activity_actor> &actor, JsonOut &jsout )
 void deserialize( cata::clone_ptr<activity_actor> &actor, JsonIn &jsin )
 {
     if( jsin.test_null() ) {
+        jsin.skip_null();
         actor = nullptr;
     } else {
         JsonObject data = jsin.get_object();
