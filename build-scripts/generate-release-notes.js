52b67f6f3e3a833cc4e923d0cfc45d6887135b52
Andrew Krieger <akrieger@users.noreply.github.com>
2024-03-20 11:04:34 -0400

    Fix referencing wrong variable name

--- build-scripts/generate-release-notes.js
+++ build-scripts/generate-release-notes.js
@@ -31,8 +31,8 @@ async function main() {
   );
 
   let previousTag = null;
-  if (latestResponse.data) {
-    for (const responseData of latestResponse.data) {
+  if (latestReleaseResponse.data) {
+    for (const responseData of latestReleaseResponse.data) {
       console.log(responseData);
       if (responseData.draft == false && responseData.prerelease == true) {
         previousTag = responseData.tag_name;
