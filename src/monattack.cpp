aljoharaKhalid/task4

Use find_point_closest_first where appropriate.

src/monattack.cpp
@@ -2139,9 +2139,7 @@ bool mattack::formblob( monster *z )
     }
 
     bool didit = false;
-    std::vector<tripoint> pts = closest_points_first( z->pos(), 1 );
-    // Don't check own tile
-    pts.erase( pts.begin() );
+    std::vector<tripoint> pts = closest_points_first( z->pos(), 1, 1 );
     creature_tracker &creatures = get_creature_tracker();
     for( const tripoint &dest : pts ) {
         Creature *critter = creatures.creature_at( dest );

src/monmove.cpp
@@ -40,6 +40,7 @@
 #include "options.h"
 #include "pathfinding.h"
 #include "pimpl.h"
+#include "point.h"
 #include "rng.h"
 #include "scent_map.h"
 #include "sounds.h"
@@ -1774,11 +1775,14 @@ bool monster::attack_at( const tripoint &p )
 
 static tripoint find_closest_stair( const tripoint &near_this, const ter_furn_flag stair_type )
 {
-    map &here = get_map();
-    for( const tripoint &candidate : closest_points_first( near_this, 10 ) ) {
-        if( here.has_flag( stair_type, candidate ) ) {
-            return candidate;
-        }
+    const map &here = get_map();
+    std::optional<tripoint> candidate = find_point_closest_first( near_this, 0, 10, [&here,
+    stair_type]( const tripoint & candidate ) {
+        return here.has_flag( stair_type, candidate );
+    } );
+
+    if( candidate != std::nullopt ) {
+        return *candidate;
     }
     // we didn't find it
     return near_this;

src/npc.cpp
@@ -1045,7 +1045,7 @@ void npc::place_on_map()
         return;
     }
 
-    for( const tripoint &p : closest_points_first( pos(), SEEX + 1 ) ) {
+    for( const tripoint &p : closest_points_first( pos(), 1, SEEX + 1 ) ) {
         if( g->is_empty( p ) ) {
             setpos( p );
             return;

src/npcmove.cpp
@@ -3175,8 +3175,7 @@ void npc::avoid_friendly_fire()
     center.y = std::round( center.y / friend_count );
     center.z = std::round( center.z / friend_count );
 
-    std::vector<tripoint> candidates = closest_points_first( pos(), 1 );
-    candidates.erase( candidates.begin() );
+    std::vector<tripoint> candidates = closest_points_first( pos(), 1, 1 );
     std::sort( candidates.begin(), candidates.end(),
     [&tar, &center]( const tripoint & l, const tripoint & r ) {
         return ( rl_dist( l, tar ) - rl_dist( l, center ) ) <

akrieger/Cataclysm-DDA

src/monattack.cpp
@@ -2142,9 +2142,7 @@ bool mattack::formblob( monster *z )
     }
 
     bool didit = false;
-    std::vector<tripoint_bub_ms> pts = closest_points_first( z->pos_bub(), 1 );
-    // Don't check own tile
-    pts.erase( pts.begin() );
+    std::vector<tripoint_bub_ms> pts = closest_points_first( z->pos_bub(), 1, 1 );
     creature_tracker &creatures = get_creature_tracker();
     for( const tripoint_bub_ms &dest : pts ) {
         Creature *critter = creatures.creature_at( dest );

src/monmove.cpp
@@ -41,6 +41,7 @@
 #include "options.h"
 #include "pathfinding.h"
 #include "pimpl.h"
+#include "point.h"
 #include "rng.h"
 #include "scent_map.h"
 #include "sounds.h"
@@ -1793,11 +1794,14 @@ bool monster::attack_at( const tripoint_bub_ms &p )
 static tripoint_bub_ms find_closest_stair( const tripoint_bub_ms &near_this,
         const ter_furn_flag stair_type )
 {
-    map &here = get_map();
-    for( const tripoint_bub_ms &candidate : closest_points_first( near_this, 10 ) ) {
-        if( here.has_flag( stair_type, candidate ) ) {
-            return candidate;
-        }
+    const map &here = get_map();
+    std::optional<tripoint_bub_ms> candidate = find_point_closest_first( near_this, 0, 10, [&here,
+    stair_type]( const tripoint_bub_ms & candidate ) {
+        return here.has_flag( stair_type, candidate );
+    } );
+
+    if( candidate != std::nullopt ) {
+        return *candidate;
     }
     // we didn't find it
     return near_this;

src/npc.cpp
@@ -1173,7 +1173,7 @@ void npc::place_on_map()
         return;
     }
 
-    for( const tripoint_bub_ms &p : closest_points_first( pos_bub(), SEEX + 1 ) ) {
+    for( const tripoint_bub_ms &p : closest_points_first( pos_bub(), 1, SEEX + 1 ) ) {
         if( g->is_empty( p ) ) {
             setpos( p );
             return;

src/npcmove.cpp
@@ -3170,8 +3170,7 @@ void npc::avoid_friendly_fire()
 
     tripoint_bub_ms center = midpoint_round_to_nearest( fr_pts );
 
-    std::vector<tripoint_bub_ms> candidates = closest_points_first( pos_bub(), 1 );
-    candidates.erase( candidates.begin() );
+    std::vector<tripoint_bub_ms> candidates = closest_points_first( pos_bub(), 1, 1 );
     std::sort( candidates.begin(), candidates.end(),
     [&tar, &center]( const tripoint_bub_ms & l, const tripoint_bub_ms & r ) {
         return ( rl_dist( l, tar ) - rl_dist( l, center ) ) <