94e6c1c990a6d27f0ac8dd9fc6d750353ce2171d
Andrew Krieger <akrieger@users.noreply.github.com>
2025-01-16 23:35:05 -0800

    android3 electric boogalee

--- android/app/jni/src/third-party/Android.mk
+++ android/app/jni/src/third-party/Android.mk
@@ -10,7 +10,7 @@ LOCAL_CPP_FEATURES := exceptions rtti
 
 # Add your application source files here...
 FLATBUFFERS_SRCS := $(sort $(wildcard $(LOCAL_PATH)/third-party/flatbuffers/*.cpp))
-ZSTD_SRCS := $(sort $(wildcard $(LOCAL_PATH/third-party/zstd/**/*.c)))
+ZSTD_SRCS := $(sort $(wildcard $(LOCAL_PATH)/third-party/zstd/common/*.c) $(wildcard $(LOCAL_PATH)/third-party/zstd/compress/*.c) $(wildcard $(LOCAL_PATH)/third-party/zstd/decompress/*.c))
 LOCAL_SRC_FILES := $(sort $(FLATBUFFERS_SRCS:$(LOCAL_PATH)/%=%) $(ZSTD_SRCS:$(LOCAL_PATH)/%=%))
 
 LOCAL_CFLAGS += -DBACKTRACE=1 -DLOCALIZE=1 -DZSTD_STATIC_LINKING_ONLY -DZSTD_DISABLE_ASM -Wextra -Wall -fsigned-char
