1bb365c836c0a08b59101b0ddaacb67418fdc292
Andrew Krieger <akrieger@users.noreply.github.com>
2025-09-30 19:59:49 -0700

    huh the interpreter works. i shouldn't be surprised.

--- src/qjs.cpp
+++ src/qjs.cpp
@@ -1,6 +1,7 @@
 #include "qjs.h"
 
 #include <qjs/quickjs.h>
+#include <qjs/quickjs-libc.h>
 
 #include "input_context.h"
 #include "ui_manager.h"
@@ -55,6 +56,45 @@ void Console::init()
 
 void Console::run()
 {
+    auto r = Runtime::make();
+
+    auto c = Context::make( r );
+
+    js_std_init_handlers( r->get() );
+
+    JS_SetModuleLoaderFunc( r->get(), nullptr, js_module_loader, nullptr );
+
+    js_std_add_helpers( c->get(), 0, nullptr );
+
+    /* system modules */
+    js_init_module_std( c->get(), "std" );
+    js_init_module_os( c->get(), "os" );
+
+    std::string script = R"(console.log(yolo,))";
+
+    auto ret = JS_Eval( c->get(), script.c_str(), script.size(), "<input>", JS_EVAL_FLAG_COMPILE_ONLY );
+
+    if( JS_IsException( ret ) ) {
+        JSValue exn = JS_GetException( c->get() );
+        JSValue m = JS_GetPropertyStr( c->get(), exn, "message" );
+        JSValue s = JS_GetPropertyStr( c->get(), exn, "stack" );
+        auto sm = JS_ToCString( c->get(), m );
+        auto ss = JS_ToCString( c->get(), s );
+        JS_FreeValue( c->get(), exn ); // Free the exception object
+        JS_FreeValue( c->get(), s ); // Free the exception object
+        JS_FreeValue( c->get(), m ); // Free the exception object
+    } else {
+        ret = JS_EvalFunction( c->get(), ret );
+        auto exn = JS_GetException( c->get() );
+        JSValue m = JS_GetPropertyStr( c->get(), exn, "message" );
+        JSValue s = JS_GetPropertyStr( c->get(), exn, "stack" );
+        auto sm = JS_ToCString( c->get(), m );
+        auto ss = JS_ToCString( c->get(), s );
+        JS_FreeValue( c->get(), exn ); // Free the exception object
+        JS_FreeValue( c->get(), s ); // Free the exception object
+        JS_FreeValue( c->get(), m ); // Free the exception object
+    }
+
     init();
 
     input_context ctxt( "HELP_KEYBINDINGS" );
@@ -77,3 +117,5 @@ void Console::run()
 }
 
 }
+
+
