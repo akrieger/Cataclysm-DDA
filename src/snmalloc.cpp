45244f62ff2f8fca945c9e5869e70105600c9047
Andrew Krieger <akrieger@users.noreply.github.com>
2026-02-04 15:33:36 -0800

    snmalloc detours

--- src/snmalloc.cpp
+++ src/snmalloc.cpp
@@ -4,18 +4,10 @@
 #ifdef __ANDROID__
 #define MALLOC_USABLE_SIZE_QUALIFIER const
 #endif
-
-#include "snmalloc/override/malloc.cc" // NOLINT(bugprone-suspicious-include)
-#include "snmalloc/override/new.cc" // NOLINT(bugprone-suspicious-include)
-
 #ifdef _WIN32
-// To statically link snmalloc without errors or overrides we have to also provide _msize_base.
-// See https://developercommunity.visualstudio.com/t/-msize-implementation/626773 for the 2019
-// feature request which would address/ameliorate this.
-// See https://github.com/microsoft/snmalloc/pull/775 for the snmalloc PR to avoid this.
-size_t _msize_base( void *ptr ) noexcept
-{
-    return snmalloc::libc::malloc_usable_size( ptr );
-}
+#include <snmalloc/windows/override/detours.cc> // NOLINT(bugprone-suspicious-include)
+#else
+#include <snmalloc/override/malloc.cc> // NOLINT(bugprone-suspicious-include)
 #endif
+#include <snmalloc/override/new.cc> // NOLINT(bugprone-suspicious-include)
 #endif
