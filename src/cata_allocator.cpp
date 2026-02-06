c600a8d6047f88d804d57ff49cf7c850de471333
akrieger <akrieger@users.noreply.github.com>
2026-02-05 19:09:24 -0800

    Use rpmalloc for improved native performance.

--- src/cata_allocator.cpp
+++ src/cata_allocator.cpp
@@ -1,6 +1,8 @@
 #include "cata_allocator.h"
 #include "cata_allocator_c.h"
 
+#include <rpmalloc/rpnew.h>
+
 #if defined(TILES) || defined(SDL_SOUND)
 #define SDL_SET_MEMORY_FUNCTIONS
 #if defined(_MSC_VER) && defined(USE_VCPKG)
--- src/cata_allocator_c.c
+++ src/cata_allocator_c.c
@@ -1,14 +1,23 @@
 #include "cata_allocator_c.h"
 
-#include <stdlib.h>
+#ifdef _MSC_VER
+// Statically overriding malloc() family seems to work on release,
+// but not debug. It's brittle anyway.
+#define ENABLE_OVERRIDE 0
+#else
+// Needs _GNU_SOURCE to get some of the defines it wants
+#define _GNU_SOURCE
+#endif
+
+#include <rpmalloc/rpmalloc.c> // NOLINT(bugprone-suspicious-include)
 
 alloc_funcs cata_get_alloc_funcs( void )
 {
     alloc_funcs funcs = {
-        .mallocp = malloc,
-        .callocp = calloc,
-        .reallocp = realloc,
-        .freep = free,
+        rpmalloc,
+        rpcalloc,
+        rprealloc,
+        rpfree,
     };
     return funcs;
 }
