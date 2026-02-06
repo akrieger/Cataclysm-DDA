1a929b5b38636b76cf6a749ee87c99ca6e9568df
akrieger <akrieger@users.noreply.github.com>
2026-02-06 12:37:42 -0800

    Use rpmalloc for improved native performance.

--- src/cata_allocator_c.c
+++ src/cata_allocator_c.c
@@ -1,14 +1,27 @@
 #include "cata_allocator_c.h"
 
-#include <stdlib.h>
+#ifdef _MSC_VER
+#define ENABLE_OVERRIDE 0
+#else
+// For posix_madvise
+#define _POSIX_C_SOURCE 200112L
+// For MAP_ANONYMOUS
+#ifdef __APPLE__
+#define _DARWIN_C_SOURCE
+#else
+#define _DEFAULT_SOURCE
+#endif
+#endif
+
+#include <rpmalloc/rpmalloc.c> // NOLINT(bugprone-suspicious-include)
 
 alloc_funcs cata_get_alloc_funcs( void )
 {
     alloc_funcs funcs = {
-        .mallocp = malloc,
-        .callocp = calloc,
-        .reallocp = realloc,
-        .freep = free,
+        rpmalloc,
+        rpcalloc,
+        rprealloc,
+        rpfree,
     };
     return funcs;
 }
