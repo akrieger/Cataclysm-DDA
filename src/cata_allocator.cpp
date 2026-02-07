d45927d87670a7ffff06fd1c5e3e08245f5c79da
Andrew Krieger <akrieger@users.noreply.github.com>
2026-02-06 17:55:02 -0800

    Use snmalloc for improved native performance.

--- src/cata_allocator.cpp
+++ src/cata_allocator.cpp
@@ -1,5 +1,15 @@
 #include "cata_allocator.h"
-#include "cata_allocator_c.h"
+
+#ifndef __SANITIZE_ADDRESS__
+#define SNMALLOC_USE_WAIT_ON_ADDRESS 1
+#ifdef __ANDROID__
+#define MALLOC_USABLE_SIZE_QUALIFIER const
+#endif
+#ifndef _WIN32
+#include <snmalloc/override/malloc.cc> // NOLINT(bugprone-suspicious-include)
+#endif
+#include <snmalloc/override/new.cc> // NOLINT(bugprone-suspicious-include)
+#endif
 
 #if defined(TILES) || defined(SDL_SOUND)
 #define SDL_SET_MEMORY_FUNCTIONS
@@ -12,13 +22,13 @@
 
 void cata::init_allocator()
 {
-    [[maybe_unused]] alloc_funcs funcs = cata_get_alloc_funcs();
 #ifdef SDL_SET_MEMORY_FUNCTIONS
+#ifndef __SANITIZE_ADDRESS__
     SDL_SetMemoryFunctions(
-        funcs.mallocp,
-        funcs.callocp,
-        funcs.reallocp,
-        funcs.freep );
+        snmalloc::libc::malloc,
+        snmalloc::libc::calloc,
+        snmalloc::libc::realloc,
+        snmalloc::libc::free );
+#endif
 #endif
 }
-
